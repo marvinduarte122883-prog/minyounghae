5d315659194d5aacfbc8101a4f201668f2ad25e3
marvinduarte122883-prog <marvinduarte122883@gmail.com>
2026-08-31 22:19:32 -0700

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-// supabaseClient.js
-const SUPABASE_URL = 'https://mrrothkiygcxspiwotjx.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_wYyisTscusEPgTmpbZGYmw_e_mTcAwW';
-
-// Make sure window.supabase is assigned using supabase.createClient
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
